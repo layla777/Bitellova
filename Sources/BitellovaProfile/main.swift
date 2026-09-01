04f7ce9fed9bbd32d798fdaa223a35e1c7da4dc5
ideguti masaya <ideguti.masaya@i-vision.ai>
2026-09-01 23:20:00 +0900

    Add symmetry profiling mode

--- Sources/BitellovaProfile/main.swift
+++ Sources/BitellovaProfile/main.swift
@@ -18,22 +18,110 @@ func playOneGame() throws {
         }
 
         let moves = game.legalMoves
-        let move = UInt64(1) << (63 - moves.leadingZeroBitCount)
+        let move =
+            UInt64(1)
+            << (63 - moves.leadingZeroBitCount)
 
         try game.play(move)
     }
 }
 
-let trialCount = 1_000_000
-let clock = ContinuousClock()
-let start = clock.now
+func profileGames(trialCount: Int) throws {
+    let clock = ContinuousClock()
+    let start = clock.now
 
-for _ in 0..<trialCount {
-    try playOneGame()
+    for _ in 0..<trialCount {
+        try playOneGame()
+    }
+
+    let elapsed = start.duration(to: clock.now)
+
+    print("\(trialCount) games")
+    print("Elapsed:", elapsed)
+    print("Average:", elapsed / trialCount)
 }
 
-let elapsed = start.duration(to: clock.now)
+@inline(never)
+func transformAllSymmetries(
+    _ bits: UInt64
+) -> UInt64 {
+    var checksum: UInt64 = 0
 
-print("\(trialCount) games")
-print("Elapsed:", elapsed)
-print("Average:", elapsed / trialCount)
+    for symmetry in BoardSymmetry.allCases {
+        checksum &+= symmetry.transform(bits)
+    }
+
+    return checksum
+}
+
+func profileSymmetries(iterations: Int) {
+    precondition(iterations > 0)
+
+    var state: UInt64 =
+        0x8123_4567_89AB_CDEF
+
+    var checksum: UInt64 = 0
+
+    let clock = ContinuousClock()
+    let start = clock.now
+
+    for _ in 0..<iterations {
+        state =
+            state
+            &* 6_364_136_223_846_793_005
+            &+ 1_442_695_040_888_963_407
+
+        checksum &+=
+            transformAllSymmetries(state)
+    }
+
+    let elapsed = start.duration(to: clock.now)
+
+    let transformCount =
+        iterations
+        * BoardSymmetry.allCases.count
+
+    print("\(transformCount) symmetry transforms")
+    print("Elapsed:", elapsed)
+    print(
+        "Average:",
+        elapsed / transformCount
+    )
+    print(
+        "Checksum:",
+        String(checksum, radix: 16)
+    )
+}
+
+let arguments =
+    CommandLine.arguments.dropFirst()
+
+let mode =
+    arguments.first ?? "games"
+
+switch mode {
+case "games":
+    let trialCount =
+        arguments.dropFirst().first
+        .flatMap(Int.init)
+        ?? 1_000_000
+
+    try profileGames(
+        trialCount: trialCount
+    )
+
+case "symmetry":
+    let iterations =
+        arguments.dropFirst().first
+        .flatMap(Int.init)
+        ?? 1_000_000
+
+    profileSymmetries(
+        iterations: iterations
+    )
+
+default:
+    fatalError(
+        "Usage: bitellova-profile [games|symmetry] [count]"
+    )
+}
